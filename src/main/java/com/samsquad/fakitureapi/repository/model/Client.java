88ca7ccd1d998ab02ba25cc141eb2e75f341c80c
Salohyy212 <zerambaysalohy@gmail.com>
2024-02-12 18:28:41 +0300

    feat: implements models

---
+++ src/main/java/com/samsquad/fakitureapi/repository/model/Client.java
@@ -0,0 +1,22 @@
+package com.samsquad.fakitureapi.repository.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int clientNumber;
+    private String companyNameClient;
+    private String clientAddress;
+    private String clientPostalCode;
+    private String clientCountry;
+}
